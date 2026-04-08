8b044727e3ad5519911cbe227a130e4273e3a4cf
AndrejLomnicky <lomnicky.andrej@gmail.com>
2026-04-08 08:18:23 +0200

    commit

---
+++ sachovnica.py
@@ -0,0 +1,14 @@
+#vytvor obrázok s rozmermi 200*300 kde sa šachovnicovo striedajú pixely
+from PIL import Image
+
+sirka, vyska = 200, 300
+img = Image.new("RGB", (sirka, vyska), "blue")
+pixels = img.load()
+
+for x in range(sirka):
+    for y in range(vyska):
+        if (x + y) % 2 == 0:
+            pixels[x, y] = (0, 0, 0)
+        else:
+            pixels[x, y] = (255, 255, 255)
+img.show()
